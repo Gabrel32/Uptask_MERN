ffcc49afe4080719dba0d4571775da2ddb843da0
Gabriel Hernandez <Alegabo70@gmail.com>
2023-09-12 19:34:15 -0400

    empezando el modelode Proyectos

--- backend/controllers/usuarioControllers.js
+++ backend/controllers/usuarioControllers.js
@@ -59,13 +59,91 @@ const autenticar = async (req, res) => {
 
 const confirmar = async (req, res) => {
     const {token} = req.params
+    const usuarioConfirmar = await Usuario.findOne({token})
+    if (!usuarioConfirmar) {
+        const error = new Error("el token es incorrecto o esta expirado")
+        return res.status(404).json({msg:error.message})
+    }
 
-    c
+    try {
+        usuarioConfirmar.confirmado = true
+        usuarioConfirmar.token = ""
+        await usuarioConfirmar.save()
+        res.json({msg:"usuario confirmado correctamente"})
+    } catch (error) {
+        console.log(error);
+    }
 }
 
+const olvidePassword = async (req, res) => {
+    const {email} = req.body
+
+    const usuario = await Usuario.findOne({email})
+    if(!usuario){
+        const error = new Error("el usuario no esta registrado")
+        return res.status(404).json({msg:error.message})
+    }
+
+    try {
+        usuario.token = generarId()
+        usuario.save()
+        res.json({msg:"Hemos enviado un email con las instrucciones a tu correo"})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const comprobarToken = async (req, res) => {
+    const {token} = req.params
+
+    const tokenValido = await Usuario.findOne({token})
+
+    if (tokenValido) {
+        res.json({msg:"token valido"})
+        
+    }
+    else{
+        const error = new Error("el token es invalido")
+        return res.status(404).json({msg:error.message})
+    }
+
+} 
+
+const nuevoPassword = async (req, res) => {
+    const {token} = req.params
+    const {password} = req.body
+
+    const usuario = await Usuario.findOne({token})
+
+    if (usuario) {
+        usuario.password = password
+        usuario.token = ""
+        try {
+            await usuario.save()
+            res.json({msg:"Ha cambiado su contraseña correctamente"})
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+    else{
+        const error = new Error("el usuario no es invalido")
+        return res.status(404).json({msg:error.message})
+    }
+}
+
+const perfil = async (req, res) => {
+    const {usuario} = req
+
+    res.json(usuario)
+}
 
 export {
     registrar,
     autenticar,
-    confirmar
+    confirmar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword,
+    perfil
 }
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,9 +1,15 @@
 import express from "express"
 import {
     autenticar,
+    comprobarToken,
     confirmar,
+    nuevoPassword,
+    olvidePassword,
+    perfil,
     registrar
 } from "../controllers/usuarioControllers.js"
+
+import checkAuth from "../middleware/checkAuth.js"
 
 // Autenicacion, registro y confirrmacion de usuarios
 
@@ -12,6 +18,9 @@ const router = express.Router()
 router.post("/",registrar)
 router.post("/login",autenticar)
 router.get("/confirmar/:token",confirmar)
+router.post("/olvide-password", olvidePassword )
+router.route("/olvide-password/:token").post(nuevoPassword).get(comprobarToken)
+router.get("/perfil" , checkAuth, perfil)
 
 
 export default router
